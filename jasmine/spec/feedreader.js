e075dc7be01225e0b6b07350982373021b7a9a21
Kelley J Kilanski <17366192+kjkilanski@users.noreply.github.com>
2018-08-11 09:46:56 -0700

    Feature - Write test suite named 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,8 +55,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function() {
-
+    describe('The Menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -73,6 +72,7 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -80,6 +80,8 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
